1da91a25102010df4d05b20a5dc00096338434d2
Randall Degges <rdegges@gmail.com>
2013-04-18 23:18:14 -0700

    Updating version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # Basic package information:
     name = 'Flask-Heroku-Cacheify',
-    version = '0.1',
+    version = '0.2',
     py_modules = ('flask_cacheify', ),
 
     # Packaging options:
